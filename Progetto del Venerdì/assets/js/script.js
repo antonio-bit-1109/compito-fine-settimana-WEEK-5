f19eac8d53bf67961ba6180f112c52e207ef49cf
antonio-bit-1109 <143196020+antonio-bit-1109@users.noreply.github.com>
2023-11-24 16:49:53 +0100

    COMPITO fino settimana
    committ

---
+++ Progetto del Venerdì/assets/js/script.js
@@ -0,0 +1,23 @@
+window.onload = () => {
+    movingEmme();
+};
+
+const buttoneHeader = document.getElementById("size-button-header");
+const header = document.getElementById("header");
+const EmmeMovimentate = document.querySelector(".js-animation");
+
+window.addEventListener("scroll", () => {
+    scrollAnimation();
+});
+
+function scrollAnimation() {
+    if (window.scrollY > 50) {
+        buttoneHeader.classList.add("animazioneBottone");
+        header.classList.add("animazioneHeader");
+    } else {
+        buttoneHeader.classList.remove("animazioneBottone");
+        header.classList.remove("animazioneHeader");
+    }
+}
+
+function movingEmme() {}
