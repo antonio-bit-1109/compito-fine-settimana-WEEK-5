63aaf7e0e4ca1b7450cc25ab58b3be468b8dee34
antonio-bit-1109 <143196020+antonio-bit-1109@users.noreply.github.com>
2023-11-26 21:04:04 +0100

    emme animate malino

--- Progetto del Venerdì/assets/js/script.js
+++ Progetto del Venerdì/assets/js/script.js
@@ -1,10 +1,5 @@
-window.onload = () => {
-    movingEmme();
-};
-
 const buttoneHeader = document.getElementById("size-button-header");
 const header = document.getElementById("header");
-const EmmeMovimentate = document.querySelectorAll(".js-animation");
 
 window.addEventListener("scroll", () => {
     scrollAnimation();
@@ -19,5 +14,79 @@ function scrollAnimation() {
         header.classList.remove("animazioneHeader");
     }
 }
+const m = document.querySelectorAll("svg g");
 
-function movingEmme(params) {}
+// const fade = setInterval(() => {
+// m.forEach((item) => {
+//     if (item.getAttribute("opacity")) {
+//         const fade2 = setInterval(() => {
+//             let randomNumber = Math.floor(Math.random() * 2);
+//             item.setAttribute("opacity", `${randomNumber}`);
+//             console.log(randomNumber);
+//         }, 3000);
+//     }
+// });
+// }, 2000);
+
+// if (m.length > 0) {
+//     const blinkingM = setInterval(() => {
+
+//     }, 1000);
+// }
+
+// m.forEach((item, index) => {
+//     if (item.getAttribute("opacity")) {
+//         const fade2 = setInterval(() => {
+//             let randomNumber = Math.floor(Math.random() * 2);
+//             item.setAttribute("opacity", `${randomNumber}`);
+//             console.log(randomNumber);
+//         }, 1000 * index); // Stagger the start times based on the index
+//     }
+// });
+
+// const m = document.querySelectorAll("svg g");
+
+// m.forEach((item, index) => {
+//     if (item.getAttribute("opacity")) {
+//         setInterval(() => {
+//             let randomNumber = Math.floor(Math.random() * 2);
+//             item.setAttribute("opacity", `${randomNumber}`);
+//             console.log(randomNumber);
+//         }, 1000); // Adjust the delay based on the index
+//     }
+// });
+
+m.forEach((item, index) => {
+    if (item.getAttribute("opacity")) {
+        item.classList.add("transition");
+        setInterval(() => {
+            let randomNumber = Math.floor(Math.random() * 2);
+            console.log(randomNumber);
+            item.setAttribute("opacity", `${randomNumber}`);
+        }, 4000);
+    }
+});
+
+// setInterval(() => {
+//     m.forEach((item, index) => {
+//         m.classList.add("transition");
+//         let singolo = item.getAttribute("opacity");
+//         let randomNumber = Math.floor(Math.random() * 2);
+//         item.setAttribute("opacity", `${randomNumber}`);
+//     });
+// }, 2000);
+
+// m.forEach((item) => {
+//     let singolo = item.getAttribute("opacity");
+// });
+
+// const m = document.querySelectorAll("svg g");
+
+// m.forEach((item) => {
+//     item.classList.add("transition"); // Add the "transition" class to each SVG element
+//     item.classList.add("inline-block");
+//     setInterval(() => {
+//         let randomNumber = Math.floor(Math.random() * 2);
+//         item.setAttribute("opacity", `${randomNumber}`);
+//     }, 2000);
+// });
